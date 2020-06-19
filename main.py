8a7b01fe4ff0fd046c859ddd3470a8bc4e2fdaa3
Gosia <gosia.bochynska@gmail.com>
2020-06-19 14:50:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#www.mojastrona.pl/
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/services")
+def about():
+    return render_template("services.html")
+
+@app.route("/reservation")
+def reservation():
+    return render_template("reservation.html")
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
+
+#Jinja2
+
+if __name__ == "__main__":
+    app.run()
